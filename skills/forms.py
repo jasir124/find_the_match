1d7a9772f399a5c989d197efd77d75b9af19d044
Jasir124 <jasirmuhammed028@gmail.com>
2026-05-07 02:31:46 +0530

    Group skills by category in dropdown menu

--- skills/forms.py
+++ skills/forms.py
@@ -9,3 +9,14 @@ class StudentSkillForm(forms.ModelForm):
             'rating': forms.NumberInput(attrs={'min': 1, 'max': 10, 'class': 'form-control'}),
             'skill': forms.Select(attrs={'class': 'form-select'})
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        grouped_choices = []
+        for cat_code, cat_name in Skill.CATEGORY_CHOICES:
+            skills_qs = Skill.objects.filter(category=cat_code).order_by('name')
+            if skills_qs.exists():
+                grouped_choices.append(
+                    (cat_name, [(s.id, s.name) for s in skills_qs])
+                )
+        self.fields['skill'].choices = [('', '---------')] + grouped_choices
